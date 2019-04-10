fba32a0d0b696d96e45f1c07f5f3f8703e3d7780
sanjay762132 <49471264+sanjay762132@users.noreply.github.com>
2019-04-10 14:02:03 +0530

    Update and rename led.c to ledblink.c

--- ledblink.c
+++ ledblink.c
@@ -8,24 +8,23 @@ int main(void)
     // Insert code
        DDRD &= ~(1<<0);
        PORTD |= (1<<0);
-        DDRD &= ~(1<<2);
+       DDRD &= ~(1<<2);
        PORTD |= (1<<2);
        DDRD |= (1<<1);
        DDRD |= (1<<3);
 
-    while(1)
-    {
+       while(1)
+       {
         if (!(PIND & 1))
-        {
+         {
             PORTD |= (1<<1);
             _delay_ms(5000);
-        }
-        else if(!(PIND & 4))
-
-        {
+         }
+          else if(!(PIND & 4))
+          {
             PORTD |= (1<<3);
-        }
-    }
+          }
+       }
 
     return 0;
 }
